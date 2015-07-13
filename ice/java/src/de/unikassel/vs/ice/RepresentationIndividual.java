22912849aa661d54f6555d16151e12af5e1ad88a
Paspartout <paspartout@fogglabs.de>
2015-07-13 14:37:05 +0200

    Initial verion of RepresentationVisitor and test

---
+++ ice/java/src/de/unikassel/vs/ice/RepresentationIndividual.java
@@ -0,0 +1,29 @@
+package de.unikassel.vs.ice;
+
+public final class RepresentationIndividual {
+
+	public static final char DELIM = ';';
+
+	private Representation representation;
+	private String dataString;
+
+	public RepresentationIndividual(final String representationString, final String dataString) {
+		this.representation = Representation.valueOf(representationString);
+		this.dataString = dataString;
+	}
+
+	public Representation getRepresentation() {
+		return representation;
+	}
+
+	public String getDataStr() {
+		return dataString;
+	}
+
+	@Override
+	public String toString() {
+		String delimStr = "";
+		delimStr += DELIM;
+		return representation.ordinal() + delimStr + dataString;
+	}
+}
---
+++ ice/java/src/de/unikassel/vs/ice/test/RepresentationVisitorTest.java
@@ -0,0 +1,47 @@
+package de.unikassel.vs.ice.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import de.unikassel.vs.ice.IceOntologyInterface;
+import de.unikassel.vs.ice.RepresentationIndividual;
+
+public class RepresentationVisitorTest {
+
+	private IceOntologyInterface oi;
+
+	@Before
+	public final void setUp() throws Exception {
+		oi = new IceOntologyInterface();
+		String path = RepresentationVisitorTest.class.getResource("Ice.owl").getPath().replace("/Ice.owl", "");
+		oi.addIRIMapper(path);
+		assertTrue(oi.addOntologyIRI("http://www.semanticweb.org/sni/ontologies/2013/7/Ice"));
+		assertTrue(oi.loadOntologies());
+		assertTrue(oi.isConsistent());
+	}
+
+	@Test
+	public final void testNoRepresentations() throws Exception {
+		String res = oi.readRepresentationsAsCsv();
+		assertTrue("No Representations are extracted to empty string", res.isEmpty());
+	}
+
+	@Test
+	public final void testSingleRepresentation() throws Exception {
+		final String dataString = "987654321";
+		final String reprString = "IntegerRep";
+		final RepresentationIndividual expectedInd = new RepresentationIndividual(reprString, dataString);
+		final String expectedLine = expectedInd.toString();
+
+		assertTrue(oi.addIndividual(dataString, reprString));
+
+		String res = oi.readRepresentationsAsCsv();
+		assertFalse("Single Representation doesnt result in empty string", res.isEmpty());
+
+		assertEquals("Single integer representation creates correct line", expectedLine, res);
+	}
+
+}
