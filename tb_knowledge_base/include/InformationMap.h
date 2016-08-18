7c88eebeb731320d71c5fe56b2f3f2215ec77fb5
Stefan Niemczyk <sni@vs.uni-kassel.de>
2016-08-18 11:16:45 +0200

    added information map

---
+++ tb_knowledge_base/include/InformationMap.h
@@ -0,0 +1,23 @@
+/*
+ * InformationMap.h
+ *
+ *  Created on: 18.08.2016
+ *      Author: sni
+ */
+
+#ifndef INCLUDE_INFORMATIONMAP_H_
+#define INCLUDE_INFORMATIONMAP_H_
+
+namespace ice
+{
+
+class InformationMap
+{
+public:
+  InformationMap();
+  virtual ~InformationMap();
+};
+
+} /* namespace ice */
+
+#endif /* INCLUDE_INFORMATIONMAP_H_ */
---
+++ tb_knowledge_base/src/InformationMap.cpp
@@ -0,0 +1,24 @@
+/*
+ * InformationMap.cpp
+ *
+ *  Created on: 18.08.2016
+ *      Author: sni
+ */
+
+#include "InformationMap.h"
+
+namespace ice
+{
+
+InformationMap::InformationMap()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+InformationMap::~InformationMap()
+{
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace ice */
