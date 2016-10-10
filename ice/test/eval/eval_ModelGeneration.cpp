ef10f2147b77168572cd563bde1153d835abef45
Stefan Niemczyk <sni@vs.uni-kassel.de>
2016-10-10 08:38:56 +0200

    small eval stuff

--- ice/test/eval/eval_ModelGeneration.cpp
+++ ice/test/eval/eval_ModelGeneration.cpp
@@ -9,14 +9,14 @@ TEST(EvalModelGeneration, simpleEvalTests)
   std::string path = "/home/sni/Desktop/eval";
   int runs = 50;
 
-//  EvalScenarios scenarios2(path + "", [&] (supplementary::ClingWrapper *asp){
-//    asp->setPredefConfiguration(supplementary::PredefinedConfigurations::tweety);
-//    asp->setRandomize("20,15");
-//  });
+  EvalScenarios scenarios2(path + "", [&] (supplementary::ClingWrapper *asp){
+    asp->setPredefConfiguration(supplementary::PredefinedConfigurations::tweety);
+    asp->setRandomize("20,15");
+  });
   //                                 global      verbose gnuplot         runs
 //  scenarios2.fuseVictimsScenario(    true,       false,  true,            runs,      false, 6, 20, 2);
 //  scenarios2.representationScenario( true,       true,  true,          runs,      2, 12, 1);
-//  scenarios2.chainScenario(          true,       true,  true,          runs,      2, 12, 1, 10, 10, 1);
+  scenarios2.chainScenario(          true,       false,  true,          runs,      10, 10, 1, 10, 10, 1);
 //  scenarios2.islandScenario(         true,       false,  false,          runs,      2, 10, 1, 10, 10, 10);
 //  scenarios2.islandScenario(         true,       false,  false,          runs,      2, 10, 1, 20, 20, 10);
 //  scenarios2.islandScenario(         true,       false,  false,          runs,      2, 10, 1, 30, 30, 10);
@@ -39,11 +39,11 @@ TEST(EvalModelGeneration, simpleEvalTests)
 //  scenarios.chainScenario(           false,      false,  false,          runs,      10, 10, 1, 1, 10, 1);
 
 
-  runs = 1;
-  EvalScenarios scenariosT(path + "", [&] (supplementary::ClingWrapper *asp){
-    asp->setPredefConfiguration(supplementary::PredefinedConfigurations::trendy);
-  });
-  TConf conf;
+//  runs = 1;
+//  EvalScenarios scenariosT(path + "", [&] (supplementary::ClingWrapper *asp){
+//    asp->setPredefConfiguration(supplementary::PredefinedConfigurations::trendy);
+//  });
+//  TConf conf;
 //  for (int i=50; i<=400; i+=50)
 //  {
 //    conf.parallelGroupsMin = 1;
@@ -57,12 +57,12 @@ TEST(EvalModelGeneration, simpleEvalTests)
 //  }
 
 //  TConf conf;
-  conf.parallelGroupsMin = 50;
-  conf.parallelGroupsMax = 300;
-  conf.parallelGrounsStep = 50;
-  conf.levels = {10,10};
-  conf.inputsMin = 1;
-  conf.inputsMax = 4;
-  conf.skipLevel = false;
-  scenariosT.transformation(false, true, runs, 50, conf);
+//  conf.parallelGroupsMin = 50;
+//  conf.parallelGroupsMax = 300;
+//  conf.parallelGrounsStep = 50;
+//  conf.levels = {10,10};
+//  conf.inputsMin = 1;
+//  conf.inputsMax = 4;
+//  conf.skipLevel = false;
+//  scenariosT.transformation(false, true, runs, 50, conf);
 }
